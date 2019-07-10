14aa80c4bdb953d4ab3eaeb6841e26e84f85b049
Jose Maturana <jlmaturanag@ug.uchile.cl>
2019-07-10 14:53:04 -0700

    Add snake_wrapper.sh

---
+++ snake_wrapper.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+echo "Please enter the number of cores to be used by snakemake"
+read -p 'Number of cores: ' cores
+
+snakemake -pr -j $cores
+snakemake --detailed-summary|awk -F '\t' '$7!~/^-/ {print $7}' > snake_cmds.txt
+snakemake --rulegraph | dot -Tsvg > dag_rules.svg
+snakemake --dag | dot -Tsvg > dag.svg
