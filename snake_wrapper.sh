6b5064bca34241f9a5feb22528f73f35e10261d5
Jose Maturana <jlmaturanag@gmail.com>
2019-09-26 08:29:37 -0300

    Update snake wrapper

--- snake_wrapper.sh
+++ snake_wrapper.sh
@@ -4,6 +4,6 @@ echo "Please enter the number of cores to be used by snakemake"
 read -p 'Number of cores: ' cores
 
 snakemake -pr -j $cores
-snakemake --detailed-summary|awk -F '\t' '$7!~/^-/ {print $7}' > snake_cmds.txt
+#snakemake --detailed-summary|awk -F '\t' '$7!~/^-/ {print $7}' > snake_cmds.txt
 snakemake --rulegraph | dot -Tsvg > dag_rules.svg
 snakemake --dag | dot -Tsvg > dag.svg
